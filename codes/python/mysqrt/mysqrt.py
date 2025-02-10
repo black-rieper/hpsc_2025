321d9787285a2a1f474511d0e3de2a9c2c2733cb
Arshavin93 <vishalssharma1712@gmail.com>
2025-02-10 16:23:39 +0530

    added convergence test

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,11 +1,13 @@
 from numpy import sqrt
 sqrt(2.0)
-x=9.0
+x=10000.0
 s=1.0
-kmax=10
+kmax=100
+tol=1.0e-14
 for k in range(kmax):
     print(f"At iteration {k} the value of s={s}")
+    s0=s
     s =0.5*(s+(x/s))
-     
-
-
+    delta_s=s-s0
+    if(abs(delta_s)/x< tol):
+        break
