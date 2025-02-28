7824858f1f638b4b8696b5378414f375b19bc125
Arshavin93 <vishalssharma1712@gmail.com>
2025-03-01 01:07:29 +0530

    improving code of  mysqrt.py

--- codes/python/mysqrt/mysqrt.py
+++ codes/python/mysqrt/mysqrt.py
@@ -1,13 +1,24 @@
-from numpy import sqrt
-sqrt(2.0)
-x=10000.0
-s=1.0
-kmax=100
-tol=1.0e-14
-for k in range(kmax):
-    print(f"At iteration {k} the value of s={s}")
-    s0=s
-    s =0.5*(s+(x/s))
-    delta_s=s-s0
-    if(abs(delta_s)/x< tol):
-        break
+def sqrt2(x, debug=False):
+    from numpy import nan
+
+    if x == 0:
+        return 0
+    elif x < 0:
+        return nan
+
+    s = 1.0
+    kmax = 100
+    tol = 1.0e-14
+
+    for k in range(kmax):
+        s0 = s
+        s = 0.5 * (s + (x / s))
+        delta_s = s - s0
+
+        if abs(delta_s) / x < tol:
+            break
+
+    if debug:
+        print(f"finally the value of function s={s:20.15f}")
+
+    return s
